6a2cf4e007c5db65a0561494c4322083aa0a639d
darkopet <darkodepetrovski@gmail.com>
2022-06-20 13:05:21 +0200

    Database

--- Database.php
+++ Database.php
@@ -10,7 +10,7 @@
         public static Database $db1;
         public function __construct() # CONSTRUCT the database connection
         {   # DSN string = defines the connection string of the database
-            $this->pdo = new PDO('mysql:host=localhost;port=3306;dbname=ceminicrm', 'phpmyadmin', 'phpmyadmindb+--+');
+            $this->pdo = new PDO('mysql:host=localhost;port=3306;dbname=ceminicrm', 'phpmyadmin', 'phpmyadmindb00+--+');
             # If the connection to the database is not succesfull:
             $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
